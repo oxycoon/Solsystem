4c50e467dab17f1db873beb725b5126a8ceecc43
Mr-Nilsen <aleksander.i.nilsen@gmail.com>
2012-09-26 14:08:36 +0200

    Updated Planet & Moon

---
+++ Moon.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Solsystem
+{
+    public class Moon : Planet
+    {
+        public Moon(Matrix[] WVP, String name, Model model, Vector3 position, float speed, float scale, double distancePlanet, float[] rotation, float[] orbitRotation)
+        {
+           
+        }
+
+        public void DrawPlanet()
+        {
+
+        }
+    }
+}
